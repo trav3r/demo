940b44284f6b6d1a3fe4cc91e998f8cdacfd1439
tRav3r <tRav3r@gmail.com>
2013-12-19 16:27:06 +0200

    Finish work demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '920c401a9b0cc3f6963c32bf002f79ff06626175a16b0d9afb8628ee68d2c2e2c5f55bd62f400610474bfbb30d9405479aaeb9a35ccf7162d60fd3b0ad479c06'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Demo::Application.routes.draw do
+  
+  get "say/hello"
+  get "say/goodbye"
+end
